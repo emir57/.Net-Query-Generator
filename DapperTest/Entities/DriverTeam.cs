3c2142a3bc989e53d874389b3437560e041c5be8
emir57 <emir.gurbuz06@hotmail.com>
2022-07-01 14:20:47 +0300

    DriverTeam

---
+++ DapperTest/Entities/DriverTeam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperTest.Entities
+{
+    public class DriverTeam
+    {
+        public string DriverName { get; set; }
+        public string TeamName { get; set; }
+    }
+}
