996d0d03ec2754e443510773434cd130672340bb
emir57 <emir.gurbuz06@hotmail.com>
2022-07-01 13:29:39 +0300

    Driver Entity

---
+++ DapperTest/Entities/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperTest.Entities
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string DriverName { get; set; }
+        public string DriverSurname { get; set; }
+        public byte DriverNumber { get; set; }
+    }
+}
