1a02fa577412a9cf46774902ff7b05d11281614d
emir57 <emir.gurbuz06@hotmail.com>
2022-07-03 15:07:46 +0300

    NoSelectedTableException

---
+++ WriteParameter/Exceptions/NoSelectedTableException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteParameter.Exceptions
+{
+    public class NoSelectedTableException : Exception
+    {
+        public NoSelectedTableException() : this("Seçili tablo yok")
+        {
+
+        }
+
+        public NoSelectedTableException(string? message) : base(message)
+        {
+        }
+
+        public NoSelectedTableException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
