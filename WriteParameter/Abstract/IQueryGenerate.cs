dc0d6435c5290f47d37330b973a2d1e1b012df16
emir57 <emir.gurbuz06@hotmail.com>
2022-07-03 14:42:16 +0300

    IQueryGenerate SelectColumn

--- WriteParameter/Abstract/IQueryGenerate.cs
+++ WriteParameter/Abstract/IQueryGenerate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace WriteParameter.Abstract
         IQueryGenerate<TEntity> GenerateInsertQuery();
         IQueryGenerate<TEntity> GenerateUpdateQuery();
         IQueryGenerate<TEntity> SetTableName(string tableName);
+        IQueryGenerate<TEntity> SelectColumn(Expression<Func<TEntity, TEntity>> predicate);
         string Generate();
     }
 }
