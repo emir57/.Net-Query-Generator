192ffd73dab3983ab969e2e53e09e502b39bd06d
emir57 <emir.gurbuz06@hotmail.com>
2022-07-03 14:27:46 +0300

    Create IQueryGenerate interface

---
+++ WriteParameter/Abstract/IQueryGenerate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteParameter.Abstract
+{
+    public interface IQueryGenerate<TEntity>
+        where TEntity : class
+    {
+        IQueryGenerate<TEntity> GenerateInsertQuery(TEntity entity);
+        IQueryGenerate<TEntity> GenerateUpdateQuery(TEntity entity);
+        string Generate();
+    }
+}
