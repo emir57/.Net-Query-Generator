a43d98868058778ea4e7f062377f0e37cafa6700
emir57 <emir.gurbuz06@hotmail.com>
2022-07-04 13:28:01 +0300

    IQueryGenerate -> GenerateGetAllFunction

--- WriteParameter/Abstract/IQueryGenerate.cs
+++ WriteParameter/Abstract/IQueryGenerate.cs
@@ -12,6 +12,7 @@ namespace WriteParameter
     {
         string GenerateInsertQuery();
         string GenerateUpdateQuery();
+        string GenerateGetAllFunction();
         IQueryGenerate<TEntity> SetTableName(string tableName);
         IQueryGenerate<TEntity> SelectColumn<TProperty>(Expression<Func<TEntity, TProperty>> predicate);
     }
