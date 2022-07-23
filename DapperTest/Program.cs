b2e2f502363cda6b00ae3c0332ba3ba0cf9b51a0
emir57 <emir.gurbuz06@hotmail.com>
2022-07-23 21:31:26 +0300

    Program.cs Regions

--- DapperTest/Program.cs
+++ DapperTest/Program.cs
@@ -21,11 +21,17 @@ using (var conn = new SqlConnection("Server=DESKTOP-HVLQH67\\SQLEXPRESS;Database
     //    entity);
     //Console.WriteLine(row);
     #endregion
+
+    #region GetAll
     var countries = await conn.QueryAsync<Country>(query);
     foreach (var country in countries)
     {
         Console.WriteLine($"{country.CountryId} {country.CountryName} {country.CountryImageUrl}");
     }
+    #endregion
+
+
+    #region FirstOrDefault
     //Console.WriteLine("-------");
     //var getCountry = await conn.QueryFirstOrDefaultAsync<Country>(query, entity);
     //Console.WriteLine($"{getCountry?.CountryId} {getCountry?.CountryName} {getCountry?.CountryImageUrl}");
@@ -33,5 +39,6 @@ using (var conn = new SqlConnection("Server=DESKTOP-HVLQH67\\SQLEXPRESS;Database
     //var driverTeams = await conn.QueryAsync<DriverTeam>("select d.DriverName,t.Name as TeamName,d.DriverSurname from drivers d, teams t where d.TeamId = t.TeamId order by TeamName asc,DriverName asc");
     //foreach (var driverTeam in driverTeams)
     //    Console.WriteLine($"{driverTeam.DriverName} {driverTeam.DriverSurname} -> {driverTeam.TeamName}");
+    #endregion
     await conn.CloseAsync();
 }
