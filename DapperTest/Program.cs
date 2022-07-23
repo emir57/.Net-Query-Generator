463f6852693152b64db8016b41a631dde83dacd7
emir57 <emir.gurbuz06@hotmail.com>
2022-07-23 20:49:35 +0300

    Fix generate get all query function

--- DapperTest/Program.cs
+++ DapperTest/Program.cs
@@ -8,25 +8,26 @@ using (var conn = new SqlConnection("Server=DESKTOP-HVLQH67\\SQLEXPRESS;Database
     string query = new QueryGenerate<Country>()
         .SelectSchema("dbo")
         .SelectTable("countries")
-        .GenerateGetByIdQuery();
-    if (conn.State != System.Data.ConnectionState.Open)
-        await conn.OpenAsync();
-    var entity = new Country() { CountryId = 13, CountryName = "Türkiye 3", CountryImageUrl = "turkey.jpg" };
+        .SelectIdColumn(x => x.CountryId)
+        .GenerateGetAllQuery();
+    //if (conn.State != System.Data.ConnectionState.Open)
+    //    await conn.OpenAsync();
+    //var entity = new Country() { CountryId = 13, CountryName = "Türkiye 3", CountryImageUrl = "turkey.jpg" };
     //int row = await conn.ExecuteAsync(
     //    query,
     //    entity);
     //Console.WriteLine(row);
-    //var countries = await conn.QueryAsync<Country>(query);
-    //foreach (var country in countries)
-    //{
-    //    Console.WriteLine($"{country.CountryId} {country.CountryName} {country.CountryImageUrl}");
-    //}
-    Console.WriteLine("-------");
-    var getCountry = await conn.QueryFirstOrDefaultAsync<Country>(query, entity);
-    Console.WriteLine($"{getCountry?.CountryId} {getCountry?.CountryName} {getCountry?.CountryImageUrl}");
-    Console.WriteLine("-------");
-    var driverTeams = await conn.QueryAsync<DriverTeam>("select d.DriverName,t.Name as TeamName,d.DriverSurname from drivers d, teams t where d.TeamId = t.TeamId order by TeamName asc,DriverName asc");
-    foreach (var driverTeam in driverTeams)
-        Console.WriteLine($"{driverTeam.DriverName} {driverTeam.DriverSurname} -> {driverTeam.TeamName}");
+    var countries = await conn.QueryAsync<Country>(query);
+    foreach (var country in countries)
+    {
+        Console.WriteLine($"{country.CountryId} {country.CountryName} {country.CountryImageUrl}");
+    }
+    //Console.WriteLine("-------");
+    //var getCountry = await conn.QueryFirstOrDefaultAsync<Country>(query, entity);
+    //Console.WriteLine($"{getCountry?.CountryId} {getCountry?.CountryName} {getCountry?.CountryImageUrl}");
+    //Console.WriteLine("-------");
+    //var driverTeams = await conn.QueryAsync<DriverTeam>("select d.DriverName,t.Name as TeamName,d.DriverSurname from drivers d, teams t where d.TeamId = t.TeamId order by TeamName asc,DriverName asc");
+    //foreach (var driverTeam in driverTeams)
+    //    Console.WriteLine($"{driverTeam.DriverName} {driverTeam.DriverSurname} -> {driverTeam.TeamName}");
     await conn.CloseAsync();
 }
--- WriteParameter/Concrete/QueryGenerate.cs
+++ WriteParameter/Concrete/QueryGenerate.cs
@@ -55,7 +55,7 @@ namespace WriteParameter
             checkTable();
             checkSchema();
             string parameters = getParametersWithId();
-            string query = String.Format($"select {_schema}.{parameters} from {_tableName}");
+            string query = String.Format($"select {parameters} from {_schema}.{_tableName}");
             return query.Replace("ı", "i");
         }
 
