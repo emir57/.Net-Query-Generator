15a05b2f54fece38f1a28d7712692379d21ef5b0
emir57 <emir.gurbuz06@hotmail.com>
2022-07-24 18:07:31 +0300

    Country model use IdColumn Attribute

--- DapperTest/Entities/Country.cs
+++ DapperTest/Entities/Country.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WriteParameter.Attributes;
 
 namespace DapperTest.Entities
 {
     public class Country
     {
+        [IdColumn]
         public int CountryId { get; set; }
         public string CountryName { get; set; }
         public string CountryImageUrl { get; set; }
