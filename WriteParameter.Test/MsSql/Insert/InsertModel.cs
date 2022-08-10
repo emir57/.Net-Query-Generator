d385f06505f5e38a3e893e9942889bb1b53751f1
emir57 <emir.gurbuz06@hotmail.com>
2022-08-10 14:24:47 +0300

    MsSqlInsertQuery

---
+++ WriteParameter.Test/MsSql/Insert/InsertModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WriteParameter.Attributes;
+
+namespace WriteParameter.Test.MsSql.Insert
+{
+    internal class InsertModel
+    {
+        [IdColumn]
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string Continent { get; set; }
+        public string Currency { get; set; }
+    }
+}
---
+++ WriteParameter.Test/MsSql/Insert/InsertTest.cs
@@ -0,0 +1,28 @@
+﻿using DapperTest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WriteParameter.MsSqlWriter;
+
+namespace WriteParameter.Test.MsSql.Insert
+{
+    public class InsertTest
+    {
+        public InsertTest()
+        {
+
+        }
+        [Fact]
+        public void MsSql_insert()
+        {
+            string query = new MsSqlQueryGenerate<InsertModel>()
+                .GenerateInsertQuery();
+
+            string result = "insert into dbo.InsertModel (CountryName,Continent,Currency) values (@CountryName,@Continent,@Currency)";
+
+            Assert.Equal(query, result);
+        }
+    }
+}
