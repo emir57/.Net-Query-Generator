eff79350b4f88ecc921c0e04e1756118feba8b97
emir57 <emir.gurbuz06@hotmail.com>
2022-08-10 15:00:38 +0300

    Refactoring tests

---
+++ WriteParameter.Test/MsSql/Command/MsSqlCommandTest.cs
@@ -0,0 +1,46 @@
+﻿using WriteParameter.MsSqlWriter;
+using WriteParameter.Test.Entities.Delete;
+using WriteParameter.Test.Entities.Insert;
+using WriteParameter.Test.Entities.Update;
+
+namespace WriteParameter.Test.MsSql.Command
+{
+    public class MsSqlCommandTest
+    {
+        public MsSqlCommandTest()
+        {
+
+        }
+        [Fact]
+        public void MsSql_insert_command()
+        {
+            string query = new MsSqlQueryGenerate<InsertModel>()
+                .GenerateInsertQuery();
+
+            string result = "insert into dbo.InsertModel (CountryName,Continent,Currency) values (@CountryName,@Continent,@Currency)";
+
+            Assert.Equal(query, result);
+        }
+        [Fact]
+        public void MsSql_update_command()
+        {
+            string query = new MsSqlQueryGenerate<UpdateModel>()
+                .GenerateUpdateQuery();
+
+            string result = "update dbo.UpdateModel set CountryName=@CountryName,Continent=@Continent,Currency=@Currency where CountryId=@CountryId";
+
+            Assert.Equal(query, result);
+        }
+        [Fact]
+        public void MsSql_delete_command()
+        {
+            string query = new MsSqlQueryGenerate<DeleteModel>()
+                .GenerateDeleteQuery();
+
+            string result = "delete from dbo.DeleteModel where CountryId=@CountryId";
+
+            Assert.Equal(query, result);
+        }
+
+    }
+}
---
+++ WriteParameter.Test/Npg/Command/NpgCommandTest.cs
@@ -0,0 +1,45 @@
+﻿using WriteParameter.NpgWriter;
+using WriteParameter.Test.Entities.Delete;
+using WriteParameter.Test.Entities.Insert;
+using WriteParameter.Test.Entities.Update;
+
+namespace WriteParameter.Test.Npg.Command
+{
+    public class NpgCommandTest
+    {
+        public NpgCommandTest()
+        {
+
+        }
+        [Fact]
+        public void Npg_insert_command()
+        {
+            string query = new NpgQueryGenerate<InsertModel>()
+                .GenerateInsertQuery();
+
+            string result = "insert into dbo.InsertModel (\"CountryName\",\"Continent\",\"Currency\") values (@CountryName,@Continent,@Currency)";
+
+            Assert.Equal(query, result);
+        }
+        [Fact]
+        public void Npg_update_command()
+        {
+            string query = new NpgQueryGenerate<UpdateModel>()
+                .GenerateUpdateQuery();
+
+            string result = "update dbo.UpdateModel set \"CountryName\"=@CountryName,\"Continent\"=@Continent,\"Currency\"=@Currency where \"CountryId\"=@CountryId";
+
+            Assert.Equal(query, result);
+        }
+        [Fact]
+        public void Npg_delete_command()
+        {
+            string query = new NpgQueryGenerate<DeleteModel>()
+                .GenerateDeleteQuery();
+
+            string result = "delete from dbo.DeleteModel where \"CountryId\"=@CountryId";
+
+            Assert.Equal(query, result);
+        }
+    }
+}
