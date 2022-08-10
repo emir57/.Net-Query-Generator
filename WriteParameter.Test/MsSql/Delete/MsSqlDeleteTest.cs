e5e85110edf1a2284c24ba9a69bc6661337ef1d5
emir57 <emir.gurbuz06@hotmail.com>
2022-08-10 14:55:29 +0300

    MsSql_delete and Npg_delete complete tests

---
+++ WriteParameter.Test/MsSql/Delete/MsSqlDeleteTest.cs
@@ -0,0 +1,23 @@
+﻿using WriteParameter.MsSqlWriter;
+using WriteParameter.Test.Entities.Delete;
+
+namespace WriteParameter.Test.MsSql.Delete
+{
+    public class MsSqlDeleteTest
+    {
+        public MsSqlDeleteTest()
+        {
+
+        }
+        [Fact]
+        public void MsSql_delete()
+        {
+            string query = new MsSqlQueryGenerate<DeleteModel>()
+                .GenerateDeleteQuery();
+
+            string result = "delete from dbo.DeleteModel where CountryId=@CountryId";
+
+            Assert.Equal(query, result);
+        }
+    }
+}
---
+++ WriteParameter.Test/Npg/Delete/NpgDeleteTest.cs
@@ -0,0 +1,23 @@
+﻿using WriteParameter.NpgWriter;
+using WriteParameter.Test.Entities.Delete;
+
+namespace WriteParameter.Test.Npg.Delete
+{
+    public class NpgDeleteTest
+    {
+        public NpgDeleteTest()
+        {
+
+        }
+        [Fact]
+        public void Npg_delete()
+        {
+            string query = new NpgQueryGenerate<DeleteModel>()
+                .GenerateDeleteQuery();
+
+            string result = "delete from dbo.DeleteModel where \"CountryId\"=@CountryId";
+
+            Assert.Equal(query, result);
+        }
+    }
+}
