7c6246c5c3a343de4ae2fde259cf544702567231
emir57 <emir.gurbuz06@hotmail.com>
2022-08-24 16:15:34 +0300

    MsSql_get_by_id_query Test

--- WriteParameter.Test/MsSql/Query/MsSqlQueryTest.cs
+++ WriteParameter.Test/MsSql/Query/MsSqlQueryTest.cs
@@ -20,5 +20,16 @@ namespace WriteParameter.Test.MsSql.Query
 
             Assert.Equal(query, result);
         }
+
+        [Theory, InlineData(1)]
+        public void MsSql_get_by_id_query(int id)
+        {
+            string query = new MsSqlQueryGenerate<BaseModel>()
+                .GenerateGetByIdQuery(id);
+
+            string result = "select CountryId as CountryId,CountryName as CountryName,Continent as Continent,Currency as Currency from dbo.BaseModel where CountryId=1";
+
+            Assert.Equal(query, result);
+        }
     }
 }
