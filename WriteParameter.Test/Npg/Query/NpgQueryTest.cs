83347c2bf8919371b32339990bde5d6d13ce4ec1
emir57 <emir.gurbuz06@hotmail.com>
2022-08-24 16:17:56 +0300

    Npg_get_by_id_query Test

--- WriteParameter.Test/Npg/Query/NpgQueryTest.cs
+++ WriteParameter.Test/Npg/Query/NpgQueryTest.cs
@@ -20,5 +20,16 @@ namespace WriteParameter.Test.Npg.Query
 
             Assert.Equal(query, result);
         }
+
+        [Theory, InlineData(1)]
+        public void Npg_get_by_id_query(int id)
+        {
+            string query = new NpgQueryGenerate<BaseModel>()
+                .GenerateGetByIdQuery(id);
+
+            string result = "select \"CountryId\" as CountryId,\"CountryName\" as CountryName,\"Continent\" as Continent,\"Currency\" as Currency from dbo.BaseModel where \"CountryId\"=1";
+
+            Assert.Equal(query, result);
+        }
     }
 }
