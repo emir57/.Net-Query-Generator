9eeb88d8a1787edee380fbf87b3feefc9b7932c4
emir57 <emir.gurbuz06@hotmail.com>
2022-08-31 15:37:56 +0300

    Create NpgAddQuery

--- WriteParameter/Extensions/WriteParameterCommandExtensions.cs
+++ WriteParameter/Extensions/WriteParameterCommandExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using WriteParameter.MsSqlWriter;
+using WriteParameter.NpgWriter;
 
 namespace WriteParameter.Extensions
 {
@@ -8,6 +9,18 @@ namespace WriteParameter.Extensions
             where T : class
         {
             MsSqlQueryGenerate<T> generator = new MsSqlQueryGenerate<T>();
+
+            if (tableName != null)
+                generator.TableName(tableName);
+            generator.SchemaName(schema);
+
+            string query = generator.GenerateInsertQuery();
+            return query;
+        }
+        public static string NpgAddQuery<T>(this T @object, string schema = "dbo", string tableName = null)
+            where T : class
+        {
+            NpgQueryGenerate<T> generator = new NpgQueryGenerate<T>();
 
             if (tableName != null)
                 generator.TableName(tableName);
