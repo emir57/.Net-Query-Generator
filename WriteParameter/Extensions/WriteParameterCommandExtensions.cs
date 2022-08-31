72b9def4209ac5f82ef2e6bb5fe27cd2d076d0e6
emir57 <emir.gurbuz06@hotmail.com>
2022-08-31 15:35:29 +0300

    Create MsSqlAddQuery Extension

---
+++ WriteParameter/Extensions/WriteParameterCommandExtensions.cs
@@ -0,0 +1,20 @@
+﻿using WriteParameter.MsSqlWriter;
+
+namespace WriteParameter.Extensions
+{
+    public static class WriteParameterCommandExtensions
+    {
+        public static string MsSqlAddQuery<T>(this T @object, string schema = "dbo", string tableName = null)
+            where T : class
+        {
+            MsSqlQueryGenerate<T> generator = new MsSqlQueryGenerate<T>();
+
+            if (tableName != null)
+                generator.TableName(tableName);
+            generator.SchemaName(schema);
+
+            string query = generator.GenerateInsertQuery();
+            return query;
+        }
+    }
+}
