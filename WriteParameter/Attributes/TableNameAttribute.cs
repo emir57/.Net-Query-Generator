44d511661a4bfbadd1cb03760ef4731853a20efd
emir57 <emir.gurbuz06@hotmail.com>
2022-09-05 13:29:09 +0300

    Create TableNameAttribute

---
+++ WriteParameter/Attributes/TableNameAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace WriteParameter.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class TableNameAttribute : Attribute
+{
+    public readonly string TableName;
+    public TableNameAttribute(string tableName)
+    {
+        TableName = tableName;
+    }
+}
