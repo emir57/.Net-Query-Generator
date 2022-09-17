df853c96779e4df21c8abd755313207dcfd2b027
emir57 <emir.gurbuz06@hotmail.com>
2022-09-17 15:09:46 +0300

    Create WriteParameterClassAttribute

--- WriteParameter/Attributes/TableNameAttribute.cs
+++ WriteParameter/Attributes/TableNameAttribute.cs
@@ -1,6 +1,5 @@
 ﻿namespace WriteParameter.Attributes;
 
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
 public class TableNameAttribute : Attribute
 {
     public readonly string TableName;
---
+++ WriteParameter/Attributes/WriteParameterClassAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace WriteParameter.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+public abstract class WriteParameterClassAttribute : WriteParameterBaseAttribute
+{
+}
